16b8155e5571399c400d08d419b371c5e0fee02f
Eladio <eladiosch@gmail.com>
2024-07-31 11:27:24 +0200

    Fixed bug in balance and changed evm version to shanghai

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,6 +21,7 @@ const config: HardhatUserConfig = {
     version: "0.8.24",
     settings: {
       optimizer: { enabled: true, runs: 200 },
+      evmVersion: "shanghai",
     },
   },
   networks: {
